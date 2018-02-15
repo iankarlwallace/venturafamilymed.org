77de76e10c30cbeec76b243bf1a50c27bb23d739
iankarlwallace <iankarlwallace@gmail.com>
2018-02-15 15:44:24 -0800

    Adding public_html templates and extraneous files that should be tracked for site.  Excluding most content.

---
+++ public_html/log/joomla_update.php
@@ -0,0 +1,67 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2017-10-02 21:36:22 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2017-10-02T21:36:22+00:00	INFO 157.145.220.3	update	Update started by user Ian Wallace (185). Old version is 3.7.5.
+2017-10-02T21:36:23+00:00	INFO 157.145.220.3	update	Downloading update file from https://s3-us-west-2.amazonaws.com/joomla-official-downloads/joomladownloads/joomla3/Joomla_3.8.0-Stable-Update_Package.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIZ6S3Q3YQHG57ZRA%2F20171002%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20171002T213653Z&X-Amz-Expires=60&X-Amz-SignedHeaders=host&X-Amz-Signature=fc65b6d0a9be4259aad86e9d4c34edf3a253adfd1f93f20657f2d4fc2b39abad.
+2017-10-02T21:36:35+00:00	INFO 157.145.220.3	update	File Joomla_3.8.0-Stable-Update_Package.zip downloaded.
+2017-10-03T03:59:24+00:00	INFO 155.186.90.59	update	Update started by user Ian Wallace (185). Old version is 3.7.5.
+2017-10-03T03:59:26+00:00	INFO 155.186.90.59	update	File Joomla_3.8.0-Stable-Update_Package.zip downloaded.
+2017-10-03T04:04:24+00:00	INFO 155.186.90.59	update	Update started by user Ian Wallace (185). Old version is 3.7.5.
+2017-10-03T04:04:25+00:00	INFO 155.186.90.59	update	File Joomla_3.8.0-Stable-Update_Package.zip downloaded.
+2017-10-03T04:05:46+00:00	INFO 155.186.90.59	update	Update started by user Ian Wallace (185). Old version is 3.7.5.
+2017-10-03T04:05:47+00:00	INFO 155.186.90.59	update	File Joomla_3.8.0-Stable-Update_Package.zip downloaded.
+2017-10-03T04:05:48+00:00	INFO 155.186.90.59	update	Starting installation of new version.
+2017-10-03T04:07:15+00:00	INFO 155.186.90.59	update	Finalising installation.
+2017-10-03T04:07:15+00:00	INFO 155.186.90.59	update	Ran query from file 3.8.0-2017-07-28. Query text: ALTER TABLE `#__fields_groups` ADD COLUMN `params` TEXT  NOT NULL  AFTER `orderi.
+2017-10-03T04:07:15+00:00	INFO 155.186.90.59	update	Ran query from file 3.8.0-2017-07-31. Query text: INSERT INTO `#__extensions` (`extension_id`, `package_id`, `name`, `type`, `elem.
+2017-10-03T04:07:15+00:00	INFO 155.186.90.59	update	Deleting removed files and folders.
+2017-10-03T04:07:15+00:00	INFO 155.186.90.59	update	Cleaning up after installation.
+2017-10-03T04:07:15+00:00	INFO 155.186.90.59	update	Update to version 3.8.0 is complete.
+2017-10-05T04:44:45+00:00	INFO 155.186.90.59	update	Update started by user Ian Wallace (185). Old version is 3.8.0.
+2017-10-05T04:44:46+00:00	INFO 155.186.90.59	update	Downloading update file from https://s3-us-west-2.amazonaws.com/joomla-official-downloads/joomladownloads/joomla3/Joomla_3.8.1-Stable-Update_Package.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIZ6S3Q3YQHG57ZRA%2F20171005%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20171005T044516Z&X-Amz-Expires=60&X-Amz-SignedHeaders=host&X-Amz-Signature=65c2cd7464ccd0c4a5de54c0cb6a508f3946d7750f683079bee745700e2c95d1.
+2017-10-05T04:45:00+00:00	INFO 155.186.90.59	update	File Joomla_3.8.1-Stable-Update_Package.zip downloaded.
+2017-10-05T05:44:13+00:00	INFO 155.186.90.59	update	Starting installation of new version.
+2017-10-05T05:45:36+00:00	INFO 155.186.90.59	update	Finalising installation.
+2017-10-05T05:45:36+00:00	INFO 155.186.90.59	update	Deleting removed files and folders.
+2017-10-05T05:45:37+00:00	INFO 155.186.90.59	update	Cleaning up after installation.
+2017-10-05T05:45:37+00:00	INFO 155.186.90.59	update	Update to version 3.8.1 is complete.
+2017-11-07T20:13:04+00:00	INFO 157.145.220.3	update	Update started by user Ian Wallace (185). Old version is 3.8.1.
+2017-11-07T20:13:10+00:00	INFO 157.145.220.3	update	Downloading update file from https://s3-us-west-2.amazonaws.com/joomla-official-downloads/joomladownloads/joomla3/Joomla_3.8.2-Stable-Update_Package.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIZ6S3Q3YQHG57ZRA%2F20171107%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20171107T201338Z&X-Amz-Expires=60&X-Amz-SignedHeaders=host&X-Amz-Signature=24e4a4e30bbfd70762bc1c9e65e7fd39a49aefd5ec01bd2319d640dcb8f065fe.
+2017-11-07T20:13:26+00:00	INFO 157.145.220.3	update	File Joomla_3.8.2-Stable-Update_Package.zip downloaded.
+2017-11-07T21:27:00+00:00	INFO 157.145.220.3	update	Starting installation of new version.
+2017-11-07T21:28:48+00:00	INFO 157.145.220.3	update	Finalising installation.
+2017-11-07T21:28:48+00:00	INFO 157.145.220.3	update	Ran query from file 3.8.2-2017-10-14. Query text: ALTER TABLE `#__content` ADD INDEX `idx_alias` (`alias`(191));.
+2017-11-07T21:28:48+00:00	INFO 157.145.220.3	update	Deleting removed files and folders.
+2017-11-07T21:28:53+00:00	INFO 157.145.220.3	update	Cleaning up after installation.
+2017-11-07T21:28:53+00:00	INFO 157.145.220.3	update	Update to version 3.8.2 is complete.
+2017-12-19T03:07:24+00:00	INFO 172.222.183.101	update	Update started by user Ian Wallace (185). Old version is 3.8.2.
+2017-12-19T03:07:25+00:00	INFO 172.222.183.101	update	Downloading update file from https://s3-us-west-2.amazonaws.com/joomla-official-downloads/joomladownloads/joomla3/Joomla_3.8.3-Stable-Update_Package.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIZ6S3Q3YQHG57ZRA%2F20171219%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20171219T030744Z&X-Amz-Expires=60&X-Amz-SignedHeaders=host&X-Amz-Signature=6ae3268ee8b6a43c82debb117e1fdcd0911ed3dee735e13c3c93f2c1ed8eb35c.
+2017-12-19T03:07:41+00:00	INFO 172.222.183.101	update	File Joomla_3.8.3-Stable-Update_Package.zip downloaded.
+2017-12-19T08:41:34+00:00	INFO 172.222.183.101	update	Starting installation of new version.
+2017-12-19T08:43:45+00:00	INFO 172.222.183.101	update	Finalising installation.
+2017-12-19T08:43:45+00:00	INFO 172.222.183.101	update	Deleting removed files and folders.
+2017-12-19T08:43:46+00:00	INFO 172.222.183.101	update	Cleaning up after installation.
+2017-12-19T08:43:46+00:00	INFO 172.222.183.101	update	Update to version 3.8.3 is complete.
+2018-02-05T16:34:57+00:00	INFO 157.145.220.3	update	Update started by user Ian Wallace (185). Old version is 3.8.3.
+2018-02-05T16:35:01+00:00	INFO 157.145.220.3	update	Downloading update file from https://s3-us-west-2.amazonaws.com/joomla-official-downloads/joomladownloads/joomla3/Joomla_3.8.4-Stable-Update_Package.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIZ6S3Q3YQHG57ZRA%2F20180205%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20180205T163512Z&X-Amz-Expires=60&X-Amz-SignedHeaders=host&X-Amz-Signature=2f327018530f2901f5e0a436d6a6972e786fea586d1fe2dbd80c18c411af1e8a.
+2018-02-05T16:35:17+00:00	INFO 157.145.220.3	update	File Joomla_3.8.4-Stable-Update_Package.zip downloaded.
+2018-02-05T19:54:13+00:00	INFO 157.145.220.3	update	Update started by user Ian Wallace (185). Old version is 3.8.3.
+2018-02-05T19:54:15+00:00	INFO 157.145.220.3	update	File Joomla_3.8.4-Stable-Update_Package.zip downloaded.
+2018-02-05T19:54:15+00:00	INFO 157.145.220.3	update	Starting installation of new version.
+2018-02-05T19:56:14+00:00	INFO 157.145.220.3	update	Finalising installation.
+2018-02-05T19:56:14+00:00	INFO 157.145.220.3	update	Ran query from file 3.8.4-2018-01-16. Query text: ALTER TABLE `#__user_keys` DROP INDEX `series_2`;.
+2018-02-05T19:56:14+00:00	INFO 157.145.220.3	update	Ran query from file 3.8.4-2018-01-16. Query text: ALTER TABLE `#__user_keys` DROP INDEX `series_3`;.
+2018-02-05T19:56:14+00:00	INFO 157.145.220.3	update	Deleting removed files and folders.
+2018-02-05T19:56:16+00:00	INFO 157.145.220.3	update	Cleaning up after installation.
+2018-02-05T19:56:16+00:00	INFO 157.145.220.3	update	Update to version 3.8.4 is complete.
+2018-02-12T18:57:08+00:00	INFO 157.145.220.3	update	Update started by user Ian Wallace (185). Old version is 3.8.4.
+2018-02-12T18:57:12+00:00	INFO 157.145.220.3	update	Downloading update file from https://s3-us-west-2.amazonaws.com/joomla-official-downloads/joomladownloads/joomla3/Joomla_3.8.5-Stable-Update_Package.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIZ6S3Q3YQHG57ZRA%2F20180212%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20180212T185721Z&X-Amz-Expires=60&X-Amz-SignedHeaders=host&X-Amz-Signature=4ed4e6a0d4a6c86c66166066a02446c9f811204cab80d2fdf9ab0f5801f3e090.
+2018-02-12T18:57:28+00:00	INFO 157.145.220.3	update	File Joomla_3.8.5-Stable-Update_Package.zip downloaded.
+2018-02-12T18:57:29+00:00	INFO 157.145.220.3	update	Starting installation of new version.
+2018-02-12T18:59:22+00:00	INFO 157.145.220.3	update	Finalising installation.
+2018-02-12T18:59:22+00:00	INFO 157.145.220.3	update	Deleting removed files and folders.
+2018-02-12T18:59:28+00:00	INFO 157.145.220.3	update	Cleaning up after installation.
+2018-02-12T18:59:28+00:00	INFO 157.145.220.3	update	Update to version 3.8.5 is complete.
