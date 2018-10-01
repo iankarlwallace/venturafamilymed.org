38743bcdfdb10fc0c7597be77bf81db0837c907a
Ian Wallace <iankarlwallace@gmail.com>
2018-10-01 14:33:05 -0700

    Login procedures erratic behavior
    Needing to wait at least a couple of seconds post login for reliable results (I picked 3 seconds)

--- backstopjs/backstop_data/engine_scripts/puppet/onBefore-login.js
+++ backstopjs/backstop_data/engine_scripts/puppet/onBefore-login.js
@@ -4,10 +4,10 @@ module.exports = async (page, scenario, vp) => {
 
   console.log('SCENARIO > headed to [' + scenario.url + ']');
   await page.goto(scenario.url,{waitUntil: "networkidle0"});
-  await page.waitFor(1000);
   await page.type('#username','resident');
   await page.type('#password','play');
 	await page.keyboard.press('Enter');
+  await page.waitFor(3000);
 
   console.log('SCENARIO > ' + scenario.label + ' FINISHED onBefore-login script');
 };
