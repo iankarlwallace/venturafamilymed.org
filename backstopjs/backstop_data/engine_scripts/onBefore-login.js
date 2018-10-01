aa7fedc04a471d0e467a51c334bf4d898de71d7d
Ian Wallace <iankarlwallace@gmail.com>
2018-10-01 13:46:52 -0700

    Fixes for login issues with timing, update for .gitignore to not report ci_reports, updates for yarn libs and more.

--- backstopjs/backstop_data/engine_scripts/onBefore-login.js
+++
@@ -1,6 +0,0 @@
-module.exports = async (page, scenario, vp) => {
-  console.log('SCENARIO > ' + scenario.label + ' START onBefore-login script');
-  await require('./loadCookies')(page, scenario);
-
-  console.log('SCENARIO > ' + scenario.label + ' FINISHED onBefore-login script');
-};
