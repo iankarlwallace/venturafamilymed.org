b721ca0a6a67fccee64a669763aeb3acfbbe7347
Ian Wallace <iankarlwallace@gmail.com>
2018-10-01 14:19:05 -0700

    Troubleshooting scripts that with intermittent failures
    Scripts don't act the same dependent on if machine has heavy load (streaming music) will transition to laptop to try and isolate.

--- backstopjs/backstop_data/engine_scripts/puppet/onBefore-login.js
+++ backstopjs/backstop_data/engine_scripts/puppet/onBefore-login.js
@@ -4,11 +4,10 @@ module.exports = async (page, scenario, vp) => {
 
   console.log('SCENARIO > headed to [' + scenario.url + ']');
   await page.goto(scenario.url,{waitUntil: "networkidle0"});
-  await page.waitFor(500);
+  await page.waitFor(1000);
   await page.type('#username','resident');
   await page.type('#password','play');
 	await page.keyboard.press('Enter');
-  await page.waitFor(500);
 
   console.log('SCENARIO > ' + scenario.label + ' FINISHED onBefore-login script');
 };
