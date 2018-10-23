a16f85a3e83137674ad765153ddfedbecd8ac94d
Ian Wallace <iankarlwallace@gmail.com>
2018-10-23 09:49:16 -0700

    Latest updates
    There is some of the tests that fail periodically for unknown reasons (probably Joomla dynamic content).
    Ref URL and Basename change
    
    Rename of the basename to vfm.org and removal of all reference URL's b/c we want to see the effect in test first and then replicate to prod.
    Behat Can Not Do Logins Correctly
    
    The site still has significant problems with logins due to no id/names in the Log in button that is on the main page and instead it brings up the #sclogin-modal which I have not coded for.  Will change in the coming days.
    Adding in restoration script for DreamHost Prod -> Test
    
    Restoration script from Dreamhost prod backups to test environment.
    Reduce Workers to 1 - single thread 
    
    Problems with running multiple workers and will try just reducing the number to see if fixes problem.
    Content Fixes for Wunderground and others
    
    Weekly content fixes for Weather Underground that doesn't work any more and other fellowship fixes.
    Fixing menus where items move based on rank, other content changes
    
    Fixing menus that have items move based on rank (formulary, behavior links, etc).  Re-gen latest tests and confirmed.
    Fix for Rotations Page/Links and Size Diff of 1 pixel
    
    Latest round of rotation pages/links/size differences for tests.
    Integration work to Jenkins CI
    
    Integration script for Jenkins CI to run both Backstop JS and Behat Scripts during CI runs.
    Runtest updated to kill background headless chrome, testing fixes
    
    Testing fixes for
    - Headless chrome kill after starting
    - Stop trying to login with behat until it can be fixed
    - write report in CI xunit style in directory
    Update specialty test, latest for yarn.lock, add XML dir
    
    Latest batch to stat collecting JUnit style xml output for Jenkins.  Backstop will generate it and Yarn will do it as well.
    CI XML Junit Dirs for Backstop/Behat
    
    Adding in .gitignore files such that CI testing results from Backstop and Behat will be ignored.

--- backstopjs/backstop_data/engine_scripts/puppet/onBefore-login.js
+++ backstopjs/backstop_data/engine_scripts/puppet/onBefore-login.js
@@ -4,6 +4,7 @@ module.exports = async (page, scenario, vp) => {
 
   console.log('SCENARIO > headed to [' + scenario.url + ']');
   await page.goto(scenario.url,{waitUntil: "networkidle0"});
+  await page.waitFor(3000);
   await page.type('#username','resident');
   await page.type('#password','play');
 	await page.keyboard.press('Enter');
