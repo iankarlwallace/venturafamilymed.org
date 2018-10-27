08475fe0e86c53b28344930823e651c912971d00
Ian Wallace <iankarlwallace@gmail.com>
2018-10-26 21:21:55 -0700

    Conversion to Using Node.js Zombie Driver
    Conversion to using Node.js Zombie driver that has a timeout of 5 seconds (i.e. all page resources should load in under 5 seconds)

--- runtests.sh
+++ runtests.sh
@@ -2,8 +2,7 @@
 # Run all test that we can for Jenkins CI
 # Should:
 # 1) Setup npm and behat environments
-# 2) Needs to run chrome as headless prior to Behat scripts
-# 3) Exit cleanly with status code of zero if successful
+# 2) Converted to using Zombie Driver (Node.js JavaScript Headless Browswer)
 
 ROOTDIR="$PWD"
 
@@ -27,10 +26,8 @@ $YARNCMD backstop test
 # Behat
 BEHATDIR="$ROOTDIR/testing/behat"
 COMPOSER="/usr/bin/composer"
-CHROME="/usr/bin/google-chrome"
-CHROMEOPTS="--disable-gpu --headless --remote-debugging-address=0.0.0.0 --remote-debugging-port=9222 --window-size=1920,1080 --disable-extensions"
-if [[ ! -x "$CHROME" || ! -x "$COMPOSER" ]]; then
-  echo "Missing composer or google-chrome?  Can't test Behat then."
+if [[ ! -x "$COMPOSER" ]]; then
+  echo "Missing composer! Can't test Behat."
   exit 1
 fi
 
@@ -40,14 +37,8 @@ if [ ! -d "$BEHATDIR" ]; then
 fi
 
 cd $BEHATDIR
-$COMPOSER install
-/usr/bin/nohup $CHROME $CHROMEOPTS > /dev/null 2>&1 &
-CHROMEPID=$!
-echo "Chrome headless running at [$CHROMEPID]"
-/usr/bin/sleep 5
+$YARNCMD upgrade
+$COMPOSER upgrade
 $BEHATDIR/bin/behat -f pretty -o std -f junit -o xml
-/usr/bin/kill -9 $CHROMEPID > /dev/null 2>&1
-echo "Chrome headless stoppd on [$CHROMEPID]"
-/usr/bin/sleep 5
 
 exit 0
