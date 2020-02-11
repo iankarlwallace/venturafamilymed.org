317232c5dc1dbc3939ceda7399756c9a67dedf6e
Ian <iankarlwallace@gmail.com>
2020-02-11 12:58:20 -0800

    Open both sheets and just dump all template rules

--- bin/css-refactor.py
+++ bin/css-refactor.py
@@ -1,8 +1,25 @@
-#!/usr/bin/python3
+#!/usr/bin/env python
 #
 # Read template.css and user.css and then chunk out *all* duplicate css definitions
 #
 
 import cssutils
 
-print('Hello World!/n') 
+BASEDIR = './public_html/templates/protostar/css/'
+TEMPLATECSS = BASEDIR + 'template.css'
+USERCSS = BASEDIR + 'user.css'
+
+def main():
+    print('Working on finding duplicate CSS between:')
+    print('Template: ' + TEMPLATECSS)
+    print('User: ' + USERCSS)
+
+    template_parsed = cssutils.parseFile(TEMPLATECSS)
+    user_parsed = cssutils.parseFile(USERCSS)
+
+    for rule in template_parsed:
+        print(rule.cssText)
+
+
+if __name__ == "__main__":
+    main()
