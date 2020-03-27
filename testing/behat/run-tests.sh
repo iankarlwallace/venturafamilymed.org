451a87ab73ce172aac27a8848ab4a58ff010f809
Ian <iankarlwallace@gmail.com>
2020-03-27 11:36:53 -0700

    Adding in directory changes so that can run from anywhere.

--- testing/behat/run-tests.sh
+++ testing/behat/run-tests.sh
@@ -3,7 +3,10 @@
 # Start google-chrome to run Mink Behat Scripts
 #
 
+CURDIR=$(pwd)
 ROOTDIR=$(dirname $(realpath $0))
+cd $ROOTDIR
+
 CHROMECMD='/opt/google/chrome/chrome --disable-gpu --remote-debugging-address=0.0.0.0 --remote-debugging-port=9222 --window-size=1920,1080 --disable-extensions'
 
 OPT=$1
@@ -37,3 +40,5 @@ else
   echo "ERROR: Couldn't find Chrome runing [$PID]"
 fi
 echo "Done."
+
+cd $CURDIR
